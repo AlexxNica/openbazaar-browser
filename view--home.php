62f02981941338b9cd07974a188c9ba11a45fba9
Austin Williams <austinwilliams123@gmail.com>
2017-05-03 22:42:37 -0700

    style Store--information

--- view--home.php
+++ view--home.php
@@ -58,31 +58,31 @@
 						<h3>Information</h3>
 						<ul>
 							<li>
-								<h4>OpenBazaar ID</h4>
+								<h5>OpenBazaar ID</h5>
 								<p>Qmai9U7856XKgDSvMFExPbQufcsc4ksG779VyG4Md5dn4J</p>
 							</li>
 							<li>
-								<h4>Website</h4>
-								<p><a href="http://urbanart.com">http://urbanart.com</a><img src=""></p>
+								<h5>Website</h5>
+								<p><a href="http://urbanart.com">http://urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
 							</li>
 							<li>
-								<h4>Email</h4>
-								<p><a href="mailto:contact@urbanart,com">contact@urbanart.com</a><img src=""></p>
+								<h5>Email</h5>
+								<p><a href="mailto:contact@urbanart.com">contact@urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
 							</li>
 							<li>
-								<h4>Twitter</h4>
-								<p><a href="https://twitter.com/@urbanart">@urbanart</a><img src=""></p>
+								<h5>Twitter</h5>
+								<p><a href="https://twitter.com/@urbanart">@urbanart</a><img src="./dist/images/icon--offsite.png"></p>
 							</li>
 							<li>
-								<h4>Facebook</h4>
-								<p><a href="https://www.facebook.com/">/urbanart</a><img src=""></p>
+								<h5>Facebook</h5>
+								<p><a href="https://www.facebook.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
 							</li>
 							<li>
-								<h4>Instagram</h4>
-								<p><a href="https://www.instagram.com/">/urbanart</a><img src=""></p>
+								<h5>Instagram</h5>
+								<p><a href="https://www.instagram.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
 							</li>
 							<li>
-								<h4>SnapChat</h4>
+								<h5>SnapChat</h5>
 								<p>urbanart</p>
 							</li>
 						</ul>
