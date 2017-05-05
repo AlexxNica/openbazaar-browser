b2ca3d27d9718127ae32ca6483b6537d4acbf6b2
Austin Williams <austinwilliams123@gmail.com>
2017-05-04 22:43:07 -0700

    style store searh and refactor for better column resizing

--- view--home.php
+++ view--home.php
@@ -27,77 +27,79 @@
 				<h1>Home</h1>
 			</div>
 			<div class="Store__body View--home">
-				<div class="Column--left">
-					<div class="StoreCard">
-						<!-- Store's header img -->
-						<div class="StoreCard__header" style="background-image: url('./dist/images/example--cat01.jpg');"">
-							<!-- Store Avatar -->
-							<img class="Avatar" src="./dist/images/example--cat-avatar.jpg">
-						</div>
-						<div class="StoreCard__body">
-							<span class="StoreCard__name">Ski Shop</span>
-							<span class="StoreCard__handle">@skiShop</span>
-							<p>
-								We sell rare and high quality art sourced from big cities around the world. Follow us.
-							</p>
-							<div class="StoreCard__bottom">
-								<div class="StoreCard__location">
-									<img src="./dist/images/icon--pin.png">
-									Seattle, WA
-								</div>
-								<div class="StoreCard__ratings">
-									<img src="./dist/images/icon--star.png">
-									4.2
-									<!-- link to store's ratings and reviews -->
-									(<a href="">192</a>)
+				<div class="flex">
+					<div class="Column--left">
+						<div class="StoreCard">
+							<!-- Store's header img -->
+							<div class="StoreCard__header" style="background-image: url('./dist/images/example--cat01.jpg');"">
+								<!-- Store Avatar -->
+								<img class="Avatar" src="./dist/images/example--cat-avatar.jpg">
+							</div>
+							<div class="StoreCard__body">
+								<span class="StoreCard__name">Ski Shop</span>
+								<span class="StoreCard__handle">@skiShop</span>
+								<p>
+									We sell rare and high quality art sourced from big cities around the world. Follow us.
+								</p>
+								<div class="StoreCard__bottom">
+									<div class="StoreCard__location">
+										<img src="./dist/images/icon--pin.png">
+										Seattle, WA
+									</div>
+									<div class="StoreCard__ratings">
+										<img src="./dist/images/icon--star.png">
+										4.2
+										<!-- link to store's ratings and reviews -->
+										(<a href="">192</a>)
+									</div>
 								</div>
 							</div>
 						</div>
+						<section class="Section--store-information">
+							<h3>Information</h3>
+							<ul>
+								<li>
+									<h5>OpenBazaar ID</h5>
+									<p>Qmai9U7856XKgDSvMFExPbQufcsc4ksG779VyG4Md5dn4J</p>
+								</li>
+								<li>
+									<h5>Website</h5>
+									<p><a href="http://urbanart.com">http://urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
+								</li>
+								<li>
+									<h5>Email</h5>
+									<p><a href="mailto:contact@urbanart.com">contact@urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
+								</li>
+								<li>
+									<h5>Twitter</h5>
+									<p><a href="https://twitter.com/@urbanart">@urbanart</a><img src="./dist/images/icon--offsite.png"></p>
+								</li>
+								<li>
+									<h5>Facebook</h5>
+									<p><a href="https://www.facebook.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
+								</li>
+								<li>
+									<h5>Instagram</h5>
+									<p><a href="https://www.instagram.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
+								</li>
+								<li>
+									<h5>SnapChat</h5>
+									<p>urbanart</p>
+								</li>
+							</ul>
+						</section> 
 					</div>
-					<section class="Section--store-information">
-						<h3>Information</h3>
-						<ul>
-							<li>
-								<h5>OpenBazaar ID</h5>
-								<p>Qmai9U7856XKgDSvMFExPbQufcsc4ksG779VyG4Md5dn4J</p>
-							</li>
-							<li>
-								<h5>Website</h5>
-								<p><a href="http://urbanart.com">http://urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
-							</li>
-							<li>
-								<h5>Email</h5>
-								<p><a href="mailto:contact@urbanart.com">contact@urbanart.com</a><img src="./dist/images/icon--offsite.png"></p>
-							</li>
-							<li>
-								<h5>Twitter</h5>
-								<p><a href="https://twitter.com/@urbanart">@urbanart</a><img src="./dist/images/icon--offsite.png"></p>
-							</li>
-							<li>
-								<h5>Facebook</h5>
-								<p><a href="https://www.facebook.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
-							</li>
-							<li>
-								<h5>Instagram</h5>
-								<p><a href="https://www.instagram.com/">/urbanart</a><img src="./dist/images/icon--offsite.png"></p>
-							</li>
-							<li>
-								<h5>SnapChat</h5>
-								<p>urbanart</p>
-							</li>
-						</ul>
-					</section> 
-				</div>
-				<div class="Column--right">
-					<section class="Section">
-						<h2>About</h2>
-						<p>
-							At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-						</p>
-						<p>
-							Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-						</p>
-					</section>
+					<div class="Column--right">
+						<section class="Section">
+							<h2>About</h2>
+							<p>
+								At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+							</p>
+							<p>
+								Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+							</p>
+						</section>
+					</div>
 				</div>
 			</div>
 		</main>
